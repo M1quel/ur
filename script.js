fc5dc915ca86d479d935ca6484e344b00644625b
Mikkel Frederiksen <5760459@rts-365.dk>
2020-10-09 13:11:08 +0200

    update

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
+let regex = /\d+/g;
 var today = new Date();
 var secondMoveAmmount = 360/60;
 var hourMoveValue = 360/12;
+var UTCHour;
+var manualOffset = false;
+var offset = 0;
+var actualHour;
+var actualMinute;
+var actualSecond;
+var TimezoneOffset = String(new Date().getTimezoneOffset());
+var timezone = TimezoneOffset.match(regex)[0];
 
 var hourCounter = document.querySelector(".hourCounterWrapper")
 var minuteCounter = document.querySelector(".minuteCounterWrapper")
 var secondCounter = document.querySelector(".secondCounterWrapper")
-
+var digitalTime = document.querySelector(".digitalTimeTag");
+console.log(new Date().getTimezoneOffset())
 setTimeout(() =>{
     setInterval(() => {
-        let hourMoveAmmount = hourMoveValue * new Date().getHours() + "deg";
+        UTCHour = new Date().getUTCHours();
+        actualHour = parseInt(new Date().getHours()) + offset;
+        actualMinute = new Date().getMinutes();
+        actualSecond = new Date().getSeconds();
+
+        
+        
+
+        var digitalHour = actualHour;
+        var digitalMinute = actualMinute;
+        var digitalSecond = actualSecond;
+
+        if(digitalHour < 10){
+            digitalHour = `0${digitalHour}`
+        }
+        if(digitalMinute < 10){
+            digitalMinute = `0${digitalMinute}`
+        }
+        if (digitalSecond < 10) {
+            digitalSecond = `0${digitalSecond}`
+        }
+        digitalTime.innerText = `${digitalHour} : ${digitalMinute} : ${digitalSecond}`
+        let hourMoveAmmount = hourMoveValue * actualHour + "deg";
         hourCounter.style.transform = `rotate(${hourMoveAmmount})`
         
         let minuteMoveAmmount = secondMoveAmmount * new Date().getMinutes() + "deg";
@@ -46,3 +78,23 @@ setTimeout(() =>{
 
     }, 1000);
 }, 1000 - new Date().getMilliseconds())
+
+
+
+let timezoneSelector = document.querySelector(".timezoneSelect");
+
+timezoneSelector.addEventListener("change", function(){
+    manualOffset = true;
+    console.log(timezoneSelector.value);
+    if (parseInt(timezoneSelector.value) < 0) {
+        offset = timezoneSelector.value/60;
+    }
+    else if (parseInt(timezoneSelector.value) == 0){
+        actualHour = UTCHour;
+    }
+    else{
+        console.log(UTCHour);
+        actualHour = parseInt(UTCHour) + parseInt((timezoneSelector.value/60));
+        console.log(actualHour);
+    }
+})
